55470acbc91abf741573fa111d44cc0a7fed0216
alokpadhi <alokkumarpadhi198@gmail.com>
2022-06-30 21:13:32 +0000

    configurations for the project level

---
+++ config/config.py
@@ -0,0 +1,68 @@
+import sys
+from pathlib import Path
+import logging
+import logging.config
+from rich.logging import RichHandler
+from nltk.corpus import stopwords
+import wandb
+
+
+BASE_DIR = Path(__file__).parent.parent.absolute()
+CONFIG_DIR = Path(BASE_DIR, "config")
+LOGS_DIR = Path(BASE_DIR, "logs")
+DATA_DIR = Path(BASE_DIR, "datasets")
+ARTIFACTS = Path(BASE_DIR, "artifacts")
+
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+ARTIFACTS.mkdir(parents=True, exist_ok=True)
+
+STOPWORDS = stopwords.words('english')
+
+# Logger
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "loggers": {
+        "root": {
+            "handlers": ["console", "info", "error"],
+            "level": logging.INFO,
+            "propagate": True,
+        },
+    },
+}
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger("root")
+logger.handlers[0] = RichHandler(markup=True)
+
+
