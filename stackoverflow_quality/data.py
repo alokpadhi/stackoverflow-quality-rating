5650a216d0a13b906a413be881069323e549a174
alokpadhi <alokkumarpadhi198@gmail.com>
2022-07-04 09:35:08 +0000

    data related code

--- stackoverflow_quality/data.py
+++ stackoverflow_quality/data.py
@@ -1,28 +1,16 @@
 from pathlib import Path
-from typing import Optional
+from typing import Tuple
 
-import math
-import re
-import string
 import json
 
 import pandas as pd
 import numpy as np
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-
 from sklearn.model_selection import train_test_split
 
-import collections
-import itertools
 from collections import Counter
 
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
 from preprocess import preprocess
 from config import config
@@ -30,7 +18,16 @@ from config.config import logger
 
 
 class LabelEncoder(object):
-    """Encode labels into unqiue ids/integers"""
+    """Encode labels into unqiue ids/integers
+
+    Usage:
+    ```python
+    # Encode labels
+    label_encoder = LabelEncoder()
+    label_encoder.fit(labels)
+    y = label_encoder.encode(labels)
+    ```
+    """
     def __init__(self, class_to_index={}):
         self.class_to_index = class_to_index or {}
         self.index_to_class = {v:k for k, v in self.class_to_index.items()}
@@ -75,11 +72,11 @@ class LabelEncoder(object):
         return cls(**kwargs)
 
 
-def prepare(datapath: str):
-    """
+def prepare(datapath: Path) -> Tuple:
+    """ Prepare the features and targets.
 
     Args:
-        datapath (str): _description_
+        datapath (Path): Path of your training data file.
     """
     logger.info(f"Reading {datapath} to a pandas dataframe.")
     train_df = pd.read_csv(datapath)
@@ -93,33 +90,30 @@ def prepare(datapath: str):
     return X, y
 
 
-def split_data(X, y, labelencoder_file: str=None, train_size: float=0.7, val_size: float=0.15, test_size: float=0.15):
-    """ Read the raw unprocessed data, process it, encode the labels, and then split into train, val and test sets.
+def split_data(X: pd.Series, y: np.ndarray, labelencoder_fp: Path=Path(config.ARTIFACTS, "label_encoder.json"), 
+                train_size: float=0.7, val_size: float=0.15, test_size: float=0.15) -> Tuple:
+    """Read the raw unprocessed data, process it, encode the labels, and then split into train, val and test sets.
 
     Args:
-        datapath (str): Location of your unprocessed raw data.
-        labelencoder_file (Optional[None]): If you have already saved your LabelEncoder Json file, then provide the location here.
-        train_size (float, optional): Training set split size. Defaults to 0.7.
+        X (pd.Series): Feature data
+        y (np.ndarray): Target data
+        labelencoder_fp (Path, optional): Location of your label_encoder saved file or where you want to save after fit the label_encoder.
+        Defaults to None.
+        train_size (float, optional): Train split size. Defaults to 0.7.
         val_size (float, optional): Validation split size. Defaults to 0.15.
         test_size (float, optional): Test split size. Defaults to 0.15.
 
     Returns:
-        Returns split datasets.
+        Tuple: _description_
     """
-    if labelencoder_file:
-        try:
-            label_encoder = LabelEncoder.load(Path(config.ARTIFACTS, labelencoder_file))
-            logger.info(f"{label_encoder} Loaded")
-        except Exception as e:
-            logger.error(e)
-    else:
-        try:
-            label_encoder = LabelEncoder()
-            label_encoder.fit(y)
-            label_encoder.save(Path(config.ARTIFACTS, "label_encoder.json"))
-            logger.info("LabelEncoder fit finished and saved.")
-        except Exception as e:
-            logger.error(e)
+
+    try:
+        label_encoder = LabelEncoder()
+        label_encoder.fit(y)
+        # label_encoder.save(Path(config.ARTIFACTS, "label_encoder.json"))
+        logger.info("LabelEncoder fit finished")
+    except Exception as e:
+        logger.error(e)
 
     # split sizes
     train_size = 0.7
@@ -141,11 +135,19 @@ def split_data(X, y, labelencoder_file: str=None, train_size: float=0.7, val_siz
 
     y_test = label_encoder.encode(y_test)
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    return X_train, y_train, X_val, y_val, X_test, y_test, label_encoder
 
 
 # Tokenizer
 class Tokenizer(object):
+    """Tokenize the texts, which means convert the text tokens into numeric values.
+    
+    ```python
+    tokenizer = Tokenizer(char_level=False, num_tokens=5000)
+    tokenizer.fit_on_texts(texts=X_train)
+    X_train = tokenizer.texts_to_sequences(X_train)
+    ```
+    """
     def __init__(self, char_level, num_tokens=None, pad_token="<PAD>",
                 oov_token="<UNK", token_to_index=None):
         self.char_level = char_level
@@ -220,8 +222,16 @@ class Tokenizer(object):
 
 
 # Pad the sequences to max_length
-def pad_sequences(sequences, max_seq_len=0):
-    """Pad sequences to max_length in sequence"""
+def pad_sequences(sequences: np.ndarray, max_seq_len: int=0) -> np.ndarray:
+    """Pad sequences to max_length in sequence
+
+    Args:
+        sequences (np.ndarray): Tokenized text sequences.
+        max_seq_len (int, optional): Maximum sequence length. Defaults to 0.
+
+    Returns:
+        np.ndarray: Returns the padded sequences with given maximum length.
+    """
     # max_seq_len = max(max_seq_len, max(len(sequence) for sequence in sequences))
     # num_classes = sequences[0].shape[-1]
     padded_sequences = np.zeros((len(sequences), max_seq_len))
@@ -235,6 +245,15 @@ def pad_sequences(sequences, max_seq_len=0):
 
 
 class Dataset(torch.utils.data.Dataset):
+    """Create `torch.utils.data.Dataset` object to use for feeding our data into our models.
+    The main purpose of Dataset is to convert our data into torch Dataset, and then convert them to DataLoader which effectively can be
+    processed through our model for training.
+
+    ```python
+    train_dataset = Dataset(X=X_train, y=y_train)
+    train_dataloader = train_dataset.create_dataloader(batch_size=batch_size)
+    ```
+    """
     def __init__(self, X, y):
         self.X = X
         self.y = y
@@ -251,7 +270,7 @@ class Dataset(torch.utils.data.Dataset):
         return [X, len(X), y]
     
     def collate_fn(self, batch):
-        batch = np.array(batch)
+        batch = np.array(batch, dtype=object)
         X = batch[:, 0]
         seq_lens = batch[:, 1]
         y = batch[:, 2]
@@ -272,9 +291,26 @@ class Dataset(torch.utils.data.Dataset):
         )
 
 
-def prepare_data_for_training(X_train, y_train, X_val, y_val, X_test, y_test, batch_size: int=32, ):
+def prepare_data_for_training(X_train: np.ndarray, y_train: np.ndarray, X_val, 
+                            y_val, X_test, y_test, batch_size: int=32, tokenizer_fp: Path=Path(config.ARTIFACTS, "tokenizer.json")) -> Tuple:
+    """Prepare the incoming split data to Torch DataLoaders which is needed for the model training.
+
+    Args:
+        X_train (np.ndarray): training features
+        y_train (np.ndarray): training targets
+        X_val (np.ndarray): validation features
+        y_val (np.ndarray): validation targets
+        X_test (np.ndarray): test features
+        y_test (np.ndarray): test targets
+        batch_size (int, optional): batch size. Defaults to 32.
+        tokenizer_fp(Path, optional): 
+
+    Returns:
+        Tuple: Returns the dataloaders and tokenizer
+    """
     tokenizer = Tokenizer(char_level=False, num_tokens=5000)
     tokenizer.fit_on_texts(texts=X_train)
+
     VOCAB_SIZE = len(tokenizer)
 
     # convert texts to sequences
@@ -290,4 +326,4 @@ def prepare_data_for_training(X_train, y_train, X_val, y_val, X_test, y_test, ba
     val_dataloader = val_dataset.create_dataloader(batch_size=batch_size)
     test_dataloader = test_dataset.create_dataloader(batch_size=batch_size)
 
-    return train_dataloader, val_dataloader, test_dataloader
+    return train_dataloader, val_dataloader, test_dataloader, tokenizer
